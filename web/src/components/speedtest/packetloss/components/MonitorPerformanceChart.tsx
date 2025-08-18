fd7b0beb3d7309d07c40c91f3670af82d41ddd66
Audionut <audionut11@gmail.com>
2025-08-18 21:28:27 +1000

    Revert "Merge branch 'tracert-graph' into my-working"
    This reverts commit b1bf97f3033a6f1167485bf3d2e3c1f4656ed767, reversing
    changes made to c40265c8fa4da9028882e492462bbfae447d0b33.

--- web/src/components/speedtest/packetloss/components/MonitorPerformanceChart.tsx
+++ web/src/components/speedtest/packetloss/components/MonitorPerformanceChart.tsx
@@ -129,8 +129,6 @@ export const MonitorPerformanceChart: React.FC<
               fontSize={11}
               axisLine={false}
               tickLine={false}
-              scale="linear"
-              domain={[0, 100]}
               label={{
                 value: "Packet Loss (%)",
                 angle: 90,
